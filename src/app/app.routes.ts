3db8cb099b577b39dcfd684149da5fa365d7cb3a
Arnav Vats <arnavar18@gmail.com>
2025-06-22 00:51:13 +0530

    Login Page Working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,26 @@
 import { Routes } from '@angular/router';
-import { Login } from './login/login';
-import { Layout } from './layout/layout';
-import { Dashboard } from './dashboard/dashboard';
+import { LoginComponent } from './pages/login/login';
+import { Layout} from './pages/layout/layout';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: '/login',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        component: Login
-    },
-    {
-        path: '',
-        component: Layout,
-        children: [
-        {
-            path: 'dashboard',
-            component: Dashboard
-        }
-        ]
-    }
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard
+      }
+    ]
+  }
 ];
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -4,6 +4,7 @@ import { UserService } from '../../service/user';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { IUserModel } from '../../model/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +15,7 @@ import { IUserModel } from '../../model/user.model';
 
 export class LoginComponent {
   userSrv = inject(UserService);
+  router = inject(Router);
   loginObj: User = { 
     emailId: '', 
     password: '' 
@@ -23,6 +25,8 @@ export class LoginComponent {
     this.userSrv.loginUser(this.loginObj).subscribe(
       (res: IUserModel) => {
         alert("User Found..Navigate inside");
+        localStorage.setItem("parkUser", JSON.stringify(res));
+        this.router.navigate(['/dashboard']);
       },
       (error: any) => {
         alert("Wrong Credentials..Please try again");
